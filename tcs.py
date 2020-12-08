b78700d301d8b34d1ee7236c3cef0bcecda236c4
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2020-12-08 13:58:03 +0530

    pip install emoji

--- tcs.py
+++ tcs.py
@@ -1,46 +1,47 @@
+import emoji
 import time
 class Direction:
   def __init__(self,color):
     self.color = color
 def current(N,E,W,S):
   print('====TRAFFIC====')
-  print('North ',N.color)
-  print('East ',E.color)
-  print('South ',S.color)
-  print('West ',W.color)
+  print('N ',emoji.emojize(':'+N.color+':'))
+  print('E ',emoji.emojize(':'+E.color+':'))
+  print('S ',emoji.emojize(':'+S.color+':'))
+  print('W ',emoji.emojize(':'+W.color+':'))
 def tcs(N,E,W,S):
   while True:
     current(N,E,W,S)
     time.sleep(20)
-    N.color = 'Orange'
+    N.color = 'orange_heart'
     current(N,E,W,S)
     time.sleep(5)
-    N.color = 'Red'
-    E.color = 'Green'
+    N.color = 'red_circle'
+    E.color = 'green_heart'
     current(N,E,W,S)
     time.sleep(20)
-    E.color = 'Orange'
+    E.color = 'orange_heart'
     current(N,E,W,S)
     time.sleep(5)
-    E.color = 'Red'
-    S.color = 'Green'
+    E.color = 'red_circle'
+    S.color = 'green_heart'
     current(N,E,W,S)
     time.sleep(20)
-    S.color = 'Orange'
+    S.color = 'orange_heart'
     current(N,E,W,S)
     time.sleep(5)
-    S.color = 'Red'
-    W.color = 'Green'
+    S.color = 'red_circle'
+    W.color = 'green_heart'
     current(N,E,W,S)
     time.sleep(20)
-    W.color = 'Orange'
+    W.color = 'orange_heart'
     current(N,E,W,S)
     time.sleep(5)
-    W.color = 'Red'
-    N.color = 'Green'
+    W.color = 'red_circle'
+    N.color = 'green_heart'
 
-N = Direction('Green')
-E = Direction('Red')
-S = Direction('Red')
-W = Direction('Red')
+N = Direction('green_heart')
+E = Direction('red_circle')
+S = Direction('red_circle')
+W = Direction('red_circle')
 tcs(N,E,W,S)
